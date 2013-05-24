3ed758a8eede81e813d8466e62a9d00c15814f78
shenhongmings@gmail.com <shenhongmings@gmail.com>
2013-05-24 14:49:18 +0800

    1。修改QRCode 默认的尺寸大小 调整为最大

--- protected/modules/Tool/views/main/main.php
+++ protected/modules/Tool/views/main/main.php
@@ -38,7 +38,7 @@ $cs -> registerScriptFile(Yii::app() -> baseUrl.'/js/tool.trigger/tools.js',CCli
 		      
     		     <div class="input-control select">
     		         <span>Size:</span>  
-    		         <select name="size"><option value="1">1</option><option value="2">2</option><option value="3" selected="">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10">10</option></select>
+    		         <select name="size"><option value="1">1</option><option value="2">2</option><option value="3">3</option><option value="4">4</option><option value="5">5</option><option value="6">6</option><option value="7">7</option><option value="8">8</option><option value="9">9</option><option value="10" selected="">10</option></select>
     		     </div>
     		     
     			<div class="input-control textarea">
