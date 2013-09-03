3da968714ec3264c95eef60c6d98c6bc4ab08821
shenhongmings@gmail.com <shenhongmings@gmail.com>
2013-09-03 18:52:38 +0800

    1.修改自动升级数据库小组件

---
+++ protected/migrations/m130903_105040_20130903.php
@@ -0,0 +1,27 @@
+<?php
+
+class m130903_105040_20130903 extends CDbMigration
+{
+	public function up()
+	{
+	    echo "m130903_105040_20130903 does not support migration up.\n";
+	    return true;
+	}
+
+	public function down()
+	{
+		echo "m130903_105040_20130903 does not support migration down.\n";
+		return true;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
