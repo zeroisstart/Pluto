e21297e5eaf9de11305927b23e92480653ca3f36
shenhongmings@gmail.com <shenhongmings@gmail.com>
2013-09-05 16:07:11 +0800

    1.修改创建数据库错误

--- protected/migrations/m130903_105040_20130903.php
+++ protected/migrations/m130903_105040_20130903.php
@@ -5,7 +5,7 @@ class m130903_105040_20130903 extends CDbMigration
     public function up ()
     {
         echo "m130903_105040_20130903 does not support migration up.\n";
-        $this->createTable('WechatRequestLog', 
+        $this->createTable('wechat_request_log', 
                 array(
                     'id' => 'pk', 
                     'txt' => 'text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT \'请求参数\'', 
@@ -17,7 +17,7 @@ class m130903_105040_20130903 extends CDbMigration
     public function down ()
     {
         echo "m130903_105040_20130903 does not support migration down.\n";
-        $this->dropTable('WechatRequestLog');
+        $this->dropTable('wechat_request_log');
         return true;
     }
 }
