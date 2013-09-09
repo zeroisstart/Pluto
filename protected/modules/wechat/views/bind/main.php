9ae7d93713302f882b12b6e1da858ab91f935766
zeroisstart <shenhongmings@gmail.com>
2013-09-09 22:43:49 +0800

    1.修改网站头

--- protected/modules/wechat/views/bind/main.php
+++ protected/modules/wechat/views/bind/main.php
@@ -2,9 +2,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,minimum-scale=1,user-scalable=no,maximum-scale=1,initial-scale=1" />
 <title>保亿会</title>
-<link href="<?php echo Yii::app() -> baseUrl?>/css/boee/wechat.css"
-	rel="stylesheet" type="text/css">
+<link href="<?php echo Yii::app() -> baseUrl?>/css/boee/wechat.css" rel="stylesheet" type="text/css">
 
 </head>
 
--- protected/modules/wechat/views/bind/success.php
+++ protected/modules/wechat/views/bind/success.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width,minimum-scale=1,user-scalable=no,maximum-scale=1,initial-scale=1" />
 <title>保亿会</title>
 <link href="<?php echo Yii::app() ->baseUrl?>/css/boee/wechat.css"
 	rel="stylesheet" type="text/css">
