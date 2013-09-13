22fc3b218acfa7a32b3ad79b24d07ecb53f3cedb
shenhongmings@gmail.com <shenhongmings@gmail.com>
2013-09-13 10:21:13 +0800

    1.上传修改是否绑定的问题

--- protected/modules/boee/views/member/list.php
+++ protected/modules/boee/views/member/list.php
@@ -1,22 +1,27 @@
 <div class="grid_form">
 	<?php
-	$this->widget ( 'components.widgets.GridView', array (
-			'dataProvider' => $dataProvider,
-			'columns' => array (
-					'name',
-					'memberid',
-					'membertype',
-					'belong',
-					'gender',
-					'idcard',
-					'located',
-					'datetime',
-					array (
-							'header' => '操作',
-							'template' => '{update}{delete}',
-							'class' => 'components.widgets.ButtonColumn' 
-					) 
-			) 
-	) )?>
+$this->widget('components.widgets.GridView', 
+        array(
+            'dataProvider' => $dataProvider, 
+            'columns' => array(
+                'name', 
+                'memberid', 
+                'membertype', 
+                'belong', 
+                'gender', 
+                'idcard', 
+                'located', 
+                'isbind' => array(
+                        'name' => '是否绑定',
+                        'value'=>'$data->isBind'
+                ),
+                'datetime', 
+                array(
+                    'header' => '操作', 
+                    'template' => '{update}{delete}', 
+                    'class' => 'components.widgets.ButtonColumn'
+                )
+            )
+        ))?>
 
 </div>
